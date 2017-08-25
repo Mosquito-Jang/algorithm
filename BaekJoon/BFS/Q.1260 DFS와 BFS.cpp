12566d0f17836a4dbcf282bf01b49f27c44dfef1
Dong Hyuk Jang <soghthe2002@naver.com>
2017-08-25 17:56:59 +0900

    2017-08-25

---
+++ BaekJoon/BFS/Q.1260 DFS와 BFS.cpp
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <queue>
+
+using namespace std;
+
+int n, m, v, s, e;
+queue <int> q;
+bool connect[1001][1001];
+bool dfs_visited[1001];
+bool bfs_visited[1001];
+
+int dfs(int f_s)
+{
+	if (f_s == v)
+	{
+		dfs_visited[f_s] = 1;
+		printf("%d ", v);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (!dfs_visited[i] && connect[f_s][i])
+		{
+			printf("%d ", i);
+			dfs_visited[i] = 1;
+			dfs(i);
+		}
+	}	
+	return 1;
+}
+
+int bfs(int f_s)
+{
+	bfs_visited[f_s] = 1;
+	q.push(f_s);
+
+	while (!q.empty())
+	{
+		int f_x = q.front();
+		q.pop();
+		printf("%d ", f_x);
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (!bfs_visited[i] && connect[f_x][i])
+			{
+				q.push(i);
+				bfs_visited[i] = 1;
+			}
+		}
+	}
+	return 1;
+}
+
+
+int main()
+{
+	scanf("%d %d %d", &n, &m, &v);
+
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d %d", &s, &e);
+		connect[s][e] = 1;
+		connect[e][s] = 1;
+	}
+
+	dfs(v);
+	printf("\n");
+	bfs(v);		
+}
---
+++ BaekJoon/그리디/Q.1049 기타줄.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int n, m, a, b, x, y;
+int min_w = 999999999;
+int min_i = 999999999;
+int price;
+
+int main()
+{
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d %d", &a, &b);
+
+		if (a < min_w) min_w = a;
+		if (b < min_i) min_i = b;
+	}
+	int quo = n / 6;
+	int rest = n % 6;
+
+	if (min_w < min_i * rest) price = (quo + 1) * min_w;
+	else if (min_w > min_i * 6) price = min_i * n;
+	else
+	{
+		price = quo * min_w;
+		price += rest * min_i;
+	}
+
+	printf("%d", price);
+}
